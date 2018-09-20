942b990c6ccf655aeeef8cccb2231f1e252bad21
Igor Alpert <ialpert@def6.com>
2018-09-20 19:02:54 -0400

    config updates

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,18 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    "^.+\\.tsx?$": "ts-jest"
   },
-  testRegex: '(/test/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: [ 'ts', 'tsx', 'js', 'jsx', 'json', 'node' ],
+  testRegex: "(/test/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   verbose: true,
   bail: true,
-  collectCoverageFrom: [ 'src/**/*.ts', '!src/**/*.d.ts' ],
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts"],
   globals: {
-    'ts-jest': {
-      tsConfigFile: path.resolve(path.join(__dirname, 'tsconfig.jest.json')),
+    "ts-jest": {
+      tsConfigFile: path.resolve(path.join(__dirname, "tsconfig.jest.json")),
       skipBabel: true
     }
   }
-}
+};
--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,40 +1,46 @@
-import { RuleTester } from 'eslint'
-import { join, resolve } from 'path'
+import { RuleTester } from "eslint";
+import { join, resolve } from "path";
 
-RuleTester.it = (text, method) => test(text.replace(/\n+/g, ' '), method)
+// RuleTester.run = (text, method) => test(text.replace(/\n+/g, ' '), method)
 
-const ruleTester = new RuleTester()
+const ruleTester = new RuleTester();
 
-const parser = resolve(join(__dirname, '../src'))
+const parser = resolve(join(__dirname, "../src"));
 
 export const runTest = (ruleName: string, tests) => {
-  const rule = require(`../src/rules/${ruleName}`)
+  const rule = require(`../src/rules/${ruleName}`);
 
   describe(ruleName, () => {
-    it('Should exist', () => {
-      expect(rule.meta).toBeTruthy()
-      expect(rule.meta.docs).toBeTruthy()
-      expect(rule.meta.docs.description).toBeTruthy()
-      expect(rule.create).toBeTruthy()
-    })
+    it("Should exist", () => {
+      expect(rule.meta).toBeTruthy();
+      expect(rule.meta.docs).toBeTruthy();
+      expect(rule.meta.docs.description).toBeTruthy();
+      expect(rule.create).toBeTruthy();
+    });
 
-    ruleTester.run(ruleName, rule, tests)
-  })
-}
+    ruleTester.run(ruleName, rule, tests);
+  });
+};
 
-export const validFactory = (name: string, head = `function validFactory()`, tail = `end function`) => (source) => ({
+export const validFactory = (
+  name: string,
+  head = `function validFactory()`,
+  tail = `end function`
+) => source => ({
   code: `${head}\n${source}\n${tail}`,
   filename: `${name}.brs`,
   options: [],
   parser
-})
+});
 
-export const invalidFactory = (name: string, head = `function invalidFactory()`, tail = `end function`) => (
-  [ source, errors ]
-) => ({
+export const invalidFactory = (
+  name: string,
+  head = `function invalidFactory()`,
+  tail = `end function`
+) => ([source, errors]) => ({
   code: `${head}\n${source}\n${tail}`,
   errors,
   filename: `${name}.brs`,
   options: [],
   parser
-})
+});
