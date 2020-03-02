5d1909052e0db425f1d42bc75c5e5c2822a134fd
Sam Heavner <samuel.heavner@mlb.com>
2020-03-01 18:41:55 -0700

    add a rule for camelcase function names

---
+++ src/rules/function-name-camelcase.ts
@@ -0,0 +1,42 @@
+/**
+ * @fileoverview Rule to find functions that do not return anything but has return type other then Void
+ * @author Igor Alpert
+ */
+
+import { Rule } from 'eslint'
+
+const CAMLECASE_PATTERN = /^_?[a-z]+[a-zA-Z0-9]*$/
+
+const meta: Rule.RuleMetaData = {
+  docs: {
+    category: 'Stylistic Issues',
+    description: 'Check that function name with is camelcase, ignoring leading `_`',
+    recommended: true,
+  },
+  fixable: 'code',
+  messages: {
+    invalid: `Function name: "{{name}}" should match expression ${CAMLECASE_PATTERN.source}`,
+  },
+  schema: [],
+}
+
+const create = (context: Rule.RuleContext) => {
+  return {
+    FunctionDeclaration(node) {
+      const { id } = node
+      const { name } = id
+
+      if (!CAMLECASE_PATTERN.test(name)) {
+        context.report({
+          data: {
+            name,
+          },
+          messageId: 'invalid',
+          node,
+        })
+      }
+    },
+  }
+}
+
+export { meta, create }
