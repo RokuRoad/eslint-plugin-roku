058a38464820ecf2ee0c5f22be573e14f8dc9fe4
Sam Heavner <samuel.heavner@mlb.com>
2020-12-17 14:52:28 -0700

    (feat): support try catch

--- tests/rules/function-name-camelcase.spec.ts
+++ tests/rules/function-name-camelcase.spec.ts
@@ -1,31 +1,32 @@
-import { invalidFactory, runTest, validFactory } from '../helpers'
+import { invalidFactory, runTest, validFactory } from "../helpers";
 
-const RULE_NAME = 'function-name-camelcase'
+const RULE_NAME = "function-name-camelcase";
 
-const valid = validFactory(RULE_NAME, '', '')
-const invalid = invalidFactory(RULE_NAME, '', '')
+const valid = validFactory(RULE_NAME, "", "");
+const invalid = invalidFactory(RULE_NAME, "", "");
 
 export const test = runTest(RULE_NAME, {
   invalid: [
     [
       `function AFunction() as Dynamic
       end function`,
-      [{ messageId: 'invalid' }],
+      [{ messageId: "invalid" }],
     ],
     [
       `function _AFunction() as Dynamic
       end function`,
-      [{ messageId: 'invalid' }],
+      [{ messageId: "invalid" }],
     ],
     [
       `function snake_case() as Dynamic
       end function`,
-      [{ messageId: 'invalid' }],
+      [{ messageId: "invalid" }],
     ],
     [
-      `sub snake_case() as Dynamic
+      `
+      sub snake_case() as Dynamic
       end sub`,
-      [{ messageId: 'invalid' }],
+      [{ messageId: "invalid" }],
     ],
   ].map(invalid),
   valid: [
@@ -34,6 +35,11 @@ export const test = runTest(RULE_NAME, {
      end function
     `,
     `function _a() as Dynamic
+        try
+            a = 3
+        catch e
+            a = 3
+        end try
         return a
       end function
     `,
@@ -54,4 +60,4 @@ export const test = runTest(RULE_NAME, {
     end sub
     `,
   ].map(valid),
-})
+});
